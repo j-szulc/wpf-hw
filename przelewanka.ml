f4bbccdef9b382ac54938dcccd0fb5a829fcb962
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2020-01-21 00:01:35 +0100

    Comments and stylistic changes to przelwanka

--- przelewanka.ml
+++ przelewanka.ml
@@ -1,6 +1,8 @@
+(* Author: Jakub Szulc *)
 
 exception Found of int
 
+(* Create a copy of [s] with [s.(i)] entry changed to [x] *)
 let (<--) s (i,x) = 
 	let s' = Array.copy s
 	in (s'.(i) <- x; s')
@@ -12,15 +14,24 @@ let rec gcd x y =
 let reachable a =
 	let array_filter f a = 
 		Array.of_list (List.filter f (Array.to_list a))
-	in let a = array_filter (fun (x,y) -> x<>0) a
+	in let a = array_filter (fun (x,_) -> x<>0) a
 	in let n = Array.length a
 	in let a_capacity = Array.map fst a
 	in let a_goal = Array.map snd a
+	(* Gcd of capacities *)
+	(* In case of n=0: gcd_=0 *)
 	in let gcd_ = Array.fold_left (fun acc x -> gcd acc x) 0 a_capacity
 	in
-	   (n=0)
-	|| ((Array.fold_left (fun acc (x,y) -> if (y=x || y=0) then true else acc) false a)
-	&&  (Array.fold_left (fun acc y   -> if (y mod gcd_ = 0) then acc else false) true a_goal))
+		(n=0) 
+	||
+	  	(
+	  		(* At least one empty or full bucket *)
+	  		(Array.fold_left (fun acc (x,y) -> if (y=x || y=0) then true else acc) false a)
+			&&
+	  		(* Volume in each bucket divisible by gcd of capacities *)
+	  		(Array.fold_left (fun acc y -> if (y mod gcd_ = 0) then acc else false) true a_goal)
+		)
+
 
 let przelewanka a =
 	try
@@ -42,7 +53,6 @@ let przelewanka a =
 			set_visited empty 0;
 			while (not (Queue.is_empty q)) && ((when_visited a_goal)=(-1)) do
 				let s = Queue.pop q
-				(*in let _ = (print_string "Visiting now \n"; print_array print_int s)*)
 				in let time = (when_visited s)
 				in let add s' =
 					if(when_visited s' = -1) then
@@ -50,26 +60,26 @@ let przelewanka a =
 							set_visited s' (time+1);
 							Queue.push s' q
 						end
-				in
-					for i=0 to (n-1) do
-							(*print_int i; print_string "\n";*)
-							if(s.(i)>0) then add (s <-- (i,0));
+				in for i=0 to (n-1) do
+						(* Pour out the water from a non-empty bucket *)
+						if(s.(i)>0) then add (s <-- (i,0));
 
-							if(s.(i)<a_capacity.(i)) then add (s <-- (i,a_capacity.(i)));
-
-							for j=0 to (n-1) do 
-								if (i<>j) && (s.(i)>0) && (s.(j)<a_capacity.(j)) then 
-									let new_sj = min a_capacity.(j) (s.(i)+s.(j))
-									in let new_si = s.(i) - ( new_sj - (s.(j)) )
-									in add (s <-- (i,new_si) <-- (j,new_sj))
-							done
-					done
+						(* Fill a non-full bucket *)
+						if(s.(i)<a_capacity.(i)) then add (s <-- (i,a_capacity.(i)));
+						
+						(* For each bucket [j], pour the water from [i] to [j] if [i] non-empty and [j] non-full *)
+						for j=0 to (n-1) do 
+							if (i<>j) && (s.(i)>0) && (s.(j)<a_capacity.(j)) then 
+								let new_sj = min a_capacity.(j) (s.(i)+s.(j))
+								in let new_si = s.(i) - ( new_sj - (s.(j)) )
+								in add (s <-- (i,new_si) <-- (j,new_sj))
+						done
+				done
 			done;
 			when_visited a_goal
 		end
 	with 
 	| (Found time) -> time
-	| e -> raise e
 
 (** TESTS **)
 (*
